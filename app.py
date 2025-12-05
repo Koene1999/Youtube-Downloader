687478a41ba69059cb5067d96d49548143e6cf32
Koene1999 <61598774+Koene1999@users.noreply.github.com>
2025-12-05 14:57:35 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -6,4 +6,5 @@ app = Flask(__name__)
 def Youtube_Downloader():
     update1 = os.system('python -m pip install --upgrade pip')
     update2 = os.system('pip install --upgrade pytubefix')
+    print('Done!')
     return f'{update1}, {update2}'
